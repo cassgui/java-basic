67d4b7853e6dd4cb5460de5a4922b51b5488836e
cassgui <cassgui@gmail.com>
2022-12-01 18:55:16 -0300

    Aula5+Lição de casa

---
+++ aula05/HelloWorld.java
@@ -0,0 +1,5 @@
+class HelloWorld {  //Declaração de classe + Nome da classe
+    public static void main(String[] args) {  //Método //public static = modificadores de acesso do método //void = tipo de retorno //main = nome do método //String[] args = Parâmetros / Argumentos
+        System.out.println("Hello World!");// Código
+    }//blocos
+}//blocos
---
+++ aula05/OlaMundo.java
@@ -0,0 +1,5 @@
+class OlaMundo {
+    public static void main(String[] asgs){
+        System.out.println("Olá Mundo!");
+    }
+}
